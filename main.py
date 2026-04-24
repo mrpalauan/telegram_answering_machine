ee7a857ec49f51297c7472c2701c6adbf6924f94
Aleksei Taktarov <alekstaktarov@yandex-team.ru>
2026-04-24 12:58:06 +0200

    Refactor main.py to use Ollama for chat completions, updating model and response handling

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from openai import OpenAI
+from ollama import chat
+from ollama import ChatResponse
 
-OLLAMA_BASE_URL = "http://localhost:11434/v1"
-ollama = OpenAI(base_url=OLLAMA_BASE_URL, api_key="anything")
-
-
-response = ollama.chat.completions.create(model="llama3.2", messages=[{"role":"user", "content": "what is 2+2+2?"}])
-print(response.choices[0].message.content)
+response: ChatResponse = chat(model='gemma3', messages=[
+  {
+    'role': 'user',
+    'content': 'Why is the sky blue?',
+  },
+])
+print(response['message']['content'])
+# or access fields directly from the response object
+print(response.message.content)
